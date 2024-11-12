2e528734200e4816e387c781b277428dfc952656
Megan <90281084+mbrandreth@users.noreply.github.com>
2024-11-12 18:08:59 -0500

    Update appearance_page.dart

--- finalproject/lib/appearance_page.dart
+++ finalproject/lib/appearance_page.dart
@@ -7,6 +7,7 @@ import 'SettingsPage.dart' as settingsmain;
 
 import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 
 class appearance_page extends StatefulWidget {
   const appearance_page({super.key});
@@ -16,6 +17,17 @@ class appearance_page extends StatefulWidget {
 }
 
 class _AppearancePageState extends State<appearance_page>{
+
+  static const List<Widget> display = <Widget>[
+    Text('Light'),
+    Text('System'),
+    Text('Dark')
+  ];
+
+  final List<bool> _selectedDisplay = <bool>[true, false, false];
+  bool vertical = false;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,12 +46,47 @@ class _AppearancePageState extends State<appearance_page>{
               Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  /*
-                  TODO:
-                    - dark mode/light mode
-                  */
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    child: Text("Change Display?\n", style: TextStyle(
+                        fontFamily: 'Lora',
+                        fontSize: 18
+                    )),
+                  ),
                 ],
               ),
+              Row(
+                children: <Widget> [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(50, 0, 30, 300),
+                    child: ToggleButtons(
+                        direction: vertical ? Axis.vertical : Axis.horizontal,
+                        onPressed: (int index){
+                          setState(() {
+                            for (int i = 0; i < 3; i++){
+                              _selectedDisplay[i] = i == index;
+                            }
+                            if (index == 0){
+                              AdaptiveTheme.of(context).setLight();
+                            }
+                            if (index == 1){
+                              AdaptiveTheme.of(context).setSystem();
+                            }
+                            if (index == 2){
+                              AdaptiveTheme.of(context).setDark();
+                            }
+                          });
+                        },
+                        constraints: BoxConstraints(
+                          minHeight: 40,
+                          minWidth: 80,
+                        ),
+                        isSelected: _selectedDisplay,
+                        children: display
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
