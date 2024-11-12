508ec60b4daa88206311da6d96526b093424154e
illxso <90281084+illxso@users.noreply.github.com>
2024-11-12 15:55:21 -0500

    Update account_page.dart

--- finalproject/lib/account_page.dart
+++ finalproject/lib/account_page.dart
@@ -6,6 +6,7 @@ import 'about_page.dart' as aboutp;
 import 'SettingsPage.dart' as settingsmain;
 
 import 'package:flutter/material.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 class account_page extends StatefulWidget {
   const account_page({super.key});
@@ -18,7 +19,33 @@ class _AccountPageState extends State<account_page>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Account", style: TextStyle(
+          fontFamily: 'Teko',
+          fontSize: 50,
+        ))
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 300),
+          child: Column (
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /*
+                  TODO:
+                    - User Name
+                    - Username & change username
+                    - Password & change password
+                  */
+                ], // end children
+              ),
+            ], // end children
+          ),
+        ),
+      ),
     );
-  }
+  } // end build
 } // end account_page
