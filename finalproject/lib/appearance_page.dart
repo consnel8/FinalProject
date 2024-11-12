72d9d8dd8f2b1ff193f24d9376dbdeee59e6f566
illxso <90281084+illxso@users.noreply.github.com>
2024-11-12 15:55:43 -0500

    Update appearance_page.dart

--- finalproject/lib/appearance_page.dart
+++ finalproject/lib/appearance_page.dart
@@ -6,6 +6,7 @@ import 'about_page.dart' as aboutp;
 import 'SettingsPage.dart' as settingsmain;
 
 import 'package:flutter/material.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 class appearance_page extends StatefulWidget {
   const appearance_page({super.key});
@@ -18,7 +19,31 @@ class _AppearancePageState extends State<appearance_page>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Appearance", style: TextStyle(
+          fontFamily: 'Teko',
+          fontSize: 50,
+        ))
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 300),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /*
+                  TODO:
+                    - dark mode/light mode
+                  */
+                ],
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
