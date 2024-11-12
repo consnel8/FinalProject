7a8ed86620c635e7458c48e8d3fad7d0234ebd70
illxso <90281084+illxso@users.noreply.github.com>
2024-11-12 15:54:56 -0500

    Update access_page.dart

--- finalproject/lib/access_page.dart
+++ finalproject/lib/access_page.dart
@@ -6,6 +6,7 @@ import 'about_page.dart' as aboutp;
 import 'SettingsPage.dart' as settingsmain;
 
 import 'package:flutter/material.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 class access_page extends StatefulWidget {
   const access_page({super.key});
@@ -18,7 +19,31 @@ class _AccessPageState extends State<access_page>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Accessibility", style: TextStyle(
+          fontFamily: 'Teko',
+          fontSize: 50,
+        ))
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 300),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  /*
+                  TODO:
+                    - text size & change
+                  */
+                ],
+              )
+            ],
+          )
+        ),
+      ),
     );
-  }
+  } // end build
 }
