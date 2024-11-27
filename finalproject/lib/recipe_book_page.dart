cc07dfc4ccaee511d979d7698de57979a883cafd
Megan <90281084+mbrandreth@users.noreply.github.com>
2024-11-27 15:42:11 -0500

    Update recipe_book_page.dart

--- finalproject/lib/recipe_book_page.dart
+++ finalproject/lib/recipe_book_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'add_recipe_page.dart';
 import 'recipe_description_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'notifications_page.dart';
 
 class RecipeBookPage extends StatefulWidget {
   const RecipeBookPage({super.key});
