c975849e884a9e75232df4ea3f2b058e6094530b
consnel8 <91153372+consnel8@users.noreply.github.com>
2024-11-29 22:57:20 -0500

    difficulty and prep time

--- finalproject/lib/add_recipe_page.dart
+++ finalproject/lib/add_recipe_page.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddRecipePage extends StatefulWidget {
   const AddRecipePage({super.key});
@@ -14,6 +14,7 @@ class _AddRecipePageState extends State<AddRecipePage> {
   final TextEditingController ingredientsController = TextEditingController();
   final TextEditingController instructionsController = TextEditingController();
   final TextEditingController imageUrlController = TextEditingController();
+  final TextEditingController prepTimeController = TextEditingController();
 
   final List<String> mealTypes = [
     'Breakfast',
@@ -23,6 +24,7 @@ class _AddRecipePageState extends State<AddRecipePage> {
     'Dessert'
   ];
   List<String> selectedMealTypes = [];
+  int selectedDifficulty = 3; // Default difficulty
   bool isLoading = false;
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -31,10 +33,11 @@ class _AddRecipePageState extends State<AddRecipePage> {
     if (nameController.text.isEmpty || ingredientsController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-            content: Text(
-          'Please fill out all required fields.',
-          style: TextStyle(fontFamily: 'Lora'),
-        )),
+          content: Text(
+            'Please fill out all required fields.',
+            style: TextStyle(fontFamily: 'Lora'),
+          ),
+        ),
       );
       return;
     }
@@ -48,9 +51,11 @@ class _AddRecipePageState extends State<AddRecipePage> {
       'image': imageUrlController.text.isNotEmpty
           ? imageUrlController.text.trim()
           : null,
+      'prepTime': prepTimeController.text.trim(),
       'mealTypes': selectedMealTypes,
-      'favorite': false, // Default favorite status
-      'createdAt': FieldValue.serverTimestamp(), // Timestamp for creation
+      'difficulty': selectedDifficulty,
+      'favorite': false,
+      'createdAt': FieldValue.serverTimestamp(),
     };
 
     setState(() {
@@ -61,19 +66,21 @@ class _AddRecipePageState extends State<AddRecipePage> {
       await _firestore.collection('recipes').add(newRecipe);
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-            content: Text(
-          'Recipe saved successfully!',
-          style: TextStyle(fontFamily: 'Lora'),
-        )),
+          content: Text(
+            'Recipe saved successfully!',
+            style: TextStyle(fontFamily: 'Lora'),
+          ),
+        ),
       );
-      Navigator.pop(context); // Return to the previous screen
+      Navigator.pop(context);
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-            content: Text(
-          'Error saving recipe: $e',
-          style: TextStyle(fontFamily: 'Lora'),
-        )),
+          content: Text(
+            'Error saving recipe: $e',
+            style: TextStyle(fontFamily: 'Lora'),
+          ),
+        ),
       );
     } finally {
       setState(() {
@@ -88,10 +95,7 @@ class _AddRecipePageState extends State<AddRecipePage> {
       appBar: AppBar(
         title: const Text(
           'Add Recipe',
-          style: TextStyle(
-            fontFamily: 'Teko',
-            fontSize: 38,
-          ),
+          style: TextStyle(fontFamily: 'Teko', fontSize: 38),
         ),
       ),
       body: isLoading
@@ -103,58 +107,90 @@ class _AddRecipePageState extends State<AddRecipePage> {
                   TextField(
                     controller: nameController,
                     decoration: const InputDecoration(
-                        labelText: 'Recipe Name',
-                        labelStyle: TextStyle(
-                          fontFamily: 'Lora',
-                        )),
+                      labelText: 'Recipe Name',
+                      labelStyle: TextStyle(fontFamily: 'Lora'),
+                    ),
                   ),
                   TextField(
                     controller: descriptionController,
                     decoration: const InputDecoration(
-                        labelText: 'Description',
-                        labelStyle: TextStyle(
-                          fontFamily: 'Lora',
-                        )),
+                      labelText: 'Description',
+                      labelStyle: TextStyle(fontFamily: 'Lora'),
+                    ),
                   ),
                   TextField(
                     controller: ingredientsController,
                     decoration: const InputDecoration(
-                        labelText: 'Ingredients (comma separated)',
-                        labelStyle: TextStyle(
-                          fontFamily: 'Lora',
-                        )),
+                      labelText: 'Ingredients (comma separated)',
+                      labelStyle: TextStyle(fontFamily: 'Lora'),
+                    ),
                   ),
                   TextField(
                     controller: instructionsController,
                     decoration: const InputDecoration(
-                        labelText: 'Instructions',
-                        labelStyle: TextStyle(
-                          fontFamily: 'Lora',
-                        )),
+                      labelText: 'Instructions',
+                      labelStyle: TextStyle(fontFamily: 'Lora'),
+                    ),
                     maxLines: 4,
                   ),
                   TextField(
                     controller: imageUrlController,
                     decoration: const InputDecoration(
-                        labelText: 'Image URL',
-                        labelStyle: TextStyle(
-                          fontFamily: 'Lora',
-                        )),
+                      labelText: 'Image URL',
+                      labelStyle: TextStyle(fontFamily: 'Lora'),
+                    ),
+                  ),
+                  TextField(
+                    controller: prepTimeController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Preparation Time (in minutes)',
+                      labelStyle: TextStyle(fontFamily: 'Lora'),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'Select Difficulty:',
+                    style: TextStyle(fontSize: 18, fontFamily: 'Teko'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      5,
+                      (index) => IconButton(
+                        icon: Icon(
+                          index < selectedDifficulty
+                              ? Icons.star
+                              : Icons.star_border,
+                          color: Colors.amber,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            selectedDifficulty = index + 1;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    '(1 = Easy, 5 = Hard)',
+                    style: TextStyle(
+                        fontFamily: 'Lora', fontSize: 14, color: Colors.grey),
                   ),
                   const SizedBox(height: 20),
                   const Text(
                     'Select Meal Types:',
-                    style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Teko'),
+                    style: TextStyle(fontSize: 18, fontFamily: 'Teko'),
                   ),
                   Column(
                     children: mealTypes.map((type) {
                       return CheckboxListTile(
-                        title: Text(type,
-                            style: TextStyle(
-                                color: Color(0xff757575), fontFamily: 'Lora')),
+                        title: Text(
+                          type,
+                          style: const TextStyle(
+                              color: Color(0xff757575), fontFamily: 'Lora'),
+                        ),
                         value: selectedMealTypes.contains(type),
                         onChanged: (bool? selected) {
                           setState(() {
@@ -171,8 +207,10 @@ class _AddRecipePageState extends State<AddRecipePage> {
                   const SizedBox(height: 20),
                   ElevatedButton(
                     onPressed: saveRecipe,
-                    child: const Text('Save Recipe',
-                        style: TextStyle(fontFamily: 'Lora')),
+                    child: const Text(
+                      'Save Recipe',
+                      style: TextStyle(fontFamily: 'Lora'),
+                    ),
                   ),
                 ],
               ),
